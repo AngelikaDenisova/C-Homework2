c79231be03000bbbbb34ce1ed81e5e6544204fb3
Angelika <anzhelika48@mail.ru>
2022-08-25 11:24:10 +0500

    Решила задачу 15

---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Давайте проверим является ли день выходным! Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7)
+{
+   Console.WriteLine("Ошибка");
+}
+
+if(number >=1 && number <= 5)
+{
+   Console.WriteLine("Нет");
+}
+if(number == 6  | number == 7)
+{
+   Console.WriteLine("Да");
+}
